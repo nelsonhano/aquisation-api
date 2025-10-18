1c086e33ca8cc7431e56c6e40fa6172a4dce0544
koinyan nelson <koinyannelson0@gmail.com>
2025-10-18 01:48:46 +0100

    add middleware package

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
-import express from 'express';
+import cookieParser from 'cookie-parser';
+import express from 'express'; 
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: (msg) => logger.info(msg.trim()) }}));
+
+import logger from '#configs/logger.js';
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  logger.info('Hello from Aquisitions Service');
+  
+  res.status(200).send('Hello from Aquisitions Service');
 });
 
 export default app;
